09a7520678044be80816fac2274fc0e03c6aa7e6
Jearom Roxas <roxasjearom@gmail.com>
2023-06-05 22:33:11 +0800

    Add OswaldFont for Typography

--- app/src/main/java/com/roxasjearom/hellovalorant/ui/theme/Type.kt
+++ app/src/main/java/com/roxasjearom/hellovalorant/ui/theme/Type.kt
@@ -2,19 +2,49 @@ package com.roxasjearom.hellovalorant.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.roxasjearom.hellovalorant.R
+
+val OswaldFont = FontFamily(
+    Font(R.font.oswald_light, FontWeight.Light),
+    Font(R.font.oswald_regular, FontWeight.Normal),
+    Font(R.font.oswald_medium, FontWeight.Medium),
+    Font(R.font.oswald_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = OswaldFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = 0.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = OswaldFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = OswaldFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = OswaldFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
