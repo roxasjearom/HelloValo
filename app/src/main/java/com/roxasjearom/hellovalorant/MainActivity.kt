73899338140c6789c4549af3890e69a6c07753ca
Jearom Roxas <roxasjearom@gmail.com>
2024-08-18 13:34:08 +0800

    Add scroll behavior for the Top App Bar

--- app/src/main/java/com/roxasjearom/hellovalorant/MainActivity.kt
+++ app/src/main/java/com/roxasjearom/hellovalorant/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -38,13 +41,16 @@ class MainActivity : ComponentActivity() {
     fun HelloValoApp() {
         HelloValoTheme {
             val navController = rememberNavController()
+            val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
             Scaffold(
+                modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
                 topBar = {
-                    TopAppBar(
+                    CenterAlignedTopAppBar(
                         title = {
                             Text(stringResource(id = R.string.app_name))
-                        }
+                        },
+                        scrollBehavior = scrollBehavior,
                     )
                 },
             ) { innerPadding ->
